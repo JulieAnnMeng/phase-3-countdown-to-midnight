c8ccc012bed6ee5096d3573500fba35cf01093fd
Julie Meng <julieannmeng@gmail.com>
2021-09-29 19:59:52 -0400

    commit tree 5c4d2e87427e79e9667820f2219a9a98d64cd5c7

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
+require 'pry'
 
-def countdown
+number = 0;
+
+def countdown number
+    while number > 0 
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep number
+    while number > 0
+        return "#{number} SECOND(S)!"
+        number -= 1
+        sleep 1
+    end
+    return "HAPPY NEW YEAR!"
 end
